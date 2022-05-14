d838da7e2942bb6d3efe8f3158c864eb22fe712c
Mehmet Magden <yasamtr@gmail.com>
2022-05-14 18:41:39 +0300

    BackUpp

---
+++ src/test/java/day06_radioButton_checkBox/C03_RadioButtonTest.java
@@ -0,0 +1,14 @@
+package day06_radioButton_checkBox;
+
+public class C03_RadioButtonTest {
+
+    /*
+    1. Bir class oluşturun : RadioButtonTest
+2. Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+-https://www.facebook.com adresine gidin
+-Cookies’i kabul edin
+-“Create an Account” button’una basin
+-“radio buttons” elementlerini locate edin
+-Secili degilse cinsiyet butonundan size uygun olani secin
+     */
+}
