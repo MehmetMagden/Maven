b0a6561bdce66e3c1f2e677b283b3643b5e53ede
Mehmet Magden <yasamtr@gmail.com>
2022-05-20 22:10:33 +0300

    BackUpp

--- src/test/java/day11_faker_file/C03_FileExist.java
+++ src/test/java/day11_faker_file/C03_FileExist.java
@@ -1,4 +1,12 @@
 package day11_faker_file;
 
+import org.junit.Test;
+
 public class C03_FileExist {
+
+    @Test
+    public void test01() {
+        System.out.println("System.getProperty(\"user.dir\") = " + System.getProperty("user.dir"));
+        System.out.println("System.getProperty(\"user.dir\") = " + System.getProperty("user.home"));
+    }
 }
