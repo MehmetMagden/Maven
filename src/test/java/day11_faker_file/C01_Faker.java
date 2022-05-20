3c1675d3f0a69ef6933a0039872945c9c746cf8b
Mehmet Magden <yasamtr@gmail.com>
2022-05-20 21:06:05 +0300

    BackUpp

--- src/test/java/day11_faker_file/C01_Faker.java
+++ src/test/java/day11_faker_file/C01_Faker.java
@@ -1,13 +1,33 @@
 package day11_faker_file;
 
+import com.github.javafaker.Faker;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class C01_Faker extends TestBase {
 
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
 
         //facebook görevini fake isimlerle yapalım
+        driver.get("https://www.facebook.com");
+        driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+        Actions actions = new Actions(driver);
+
+        Faker faker = new Faker();
+
+        actions.click(driver.findElement(By.xpath("//input[@class='inputtext _58mg _5dba _2ph-']"))).sendKeys("Mehmet"+ Keys.TAB+"Magden"+Keys.TAB+0654654+Keys.TAB+Keys.TAB+"asdfsdaf456"+Keys.TAB+19+
+                Keys.TAB+Keys.ARROW_DOWN+Keys.TAB+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+
+                Keys.TAB+Keys.SPACE+Keys.SPACE+Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.SPACE).perform();
+
+
+
+
+        Thread.sleep(2000000);
+
+
     }
 }
