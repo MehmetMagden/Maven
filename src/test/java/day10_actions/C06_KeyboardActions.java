f35f206b64c135306a48937b039cfb53b6c95166
Mehmet Magden <yasamtr@gmail.com>
2022-05-20 21:00:54 +0300

    BackUpp

--- src/test/java/day10_actions/C06_KeyboardActions.java
+++ src/test/java/day10_actions/C06_KeyboardActions.java
@@ -19,7 +19,9 @@ public class C06_KeyboardActions extends TestBase {
         driver.get("https://www.facebook.com");
         driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
         Actions actions = new Actions(driver);
-        actions.click(driver.findElement(By.xpath("//input[@class='inputtext _58mg _5dba _2ph-']"))).sendKeys("Mehmet"+ Keys.TAB+"Magden"+Keys.TAB+0654654+Keys.TAB+"asdfsdaf456"+Keys.TAB+19).perform();
+        actions.click(driver.findElement(By.xpath("//input[@class='inputtext _58mg _5dba _2ph-']"))).sendKeys("Mehmet"+ Keys.TAB+"Magden"+Keys.TAB+0654654+Keys.TAB+Keys.TAB+"asdfsdaf456"+Keys.TAB+19+
+                Keys.TAB+Keys.ARROW_DOWN+Keys.TAB+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+Keys.ARROW_DOWN+
+                Keys.TAB+Keys.SPACE+Keys.SPACE+Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.SPACE).perform();
 
 
 
---
+++ src/test/java/day11_faker_file/C01_Faker.java
@@ -0,0 +1,13 @@
+package day11_faker_file;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C01_Faker extends TestBase {
+
+    @Test
+    public void test01() {
+
+        //facebook görevini fake isimlerle yapalım
+    }
+}
