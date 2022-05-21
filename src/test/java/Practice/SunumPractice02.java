9b42914b9e0b3e29566dd8c3971f7e259eb5c726
Mehmet Magden <yasamtr@gmail.com>
2022-05-21 13:28:49 +0300

    BackUpp

---
+++ src/test/java/Practice/SunumPractice02.java
@@ -0,0 +1,27 @@
+package Practice;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class SunumPractice02 extends TestBase {
+    @Test
+    public void test01() {
+
+
+    //1. Yeni bir Class olusturalim.C05_NavigationMethods
+    //2. Youtube ana sayfasina gidelim . https://www.youtube.com/
+        driver.get("https://www.youtube.com/");
+    //3. Amazon soyfasina gidelim. https://www.amazon.com/
+        driver.navigate().to("https://www.amazon.com/");
+    //4. Tekrar YouTube’sayfasina donelim
+        driver.navigate().back();
+    //5. Yeniden Amazon sayfasina gidelim
+        driver.navigate().forward();
+    //6. Sayfayi Refresh(yenile) yapalim
+        driver.navigate().refresh();
+    //7. Sayfayi kapatalim / Tum sayfalari kapatalim
+        //otomatik
+
+    }
+
+}
