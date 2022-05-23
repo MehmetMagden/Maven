917cf819a371f988e77919efcb6191645fab5fcd
Mehmet Magden <yasamtr@gmail.com>
2022-05-23 20:51:19 +0300

    BackUpp

--- src/test/java/day12_senkronizasyon/C02_ExplicitlyWait.java
+++ src/test/java/day12_senkronizasyon/C02_ExplicitlyWait.java
@@ -1,4 +1,38 @@
 package day12_senkronizasyon;
 
-public class C02_ExplicitlyWait {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C02_ExplicitlyWait extends TestBase {
+
+    @Test
+    public void enableTest() {
+        //1. Bir class olusturun : EnableTest
+        //2. Bir metod olusturun : isEnabled()
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        //4. Textbox’in etkin olmadigini(enabled) dogrulayın
+        WebElement enableBox = driver.findElement(By.xpath("//input[@type='text']"));
+        Assert.assertFalse(enableBox.isEnabled());
+
+        //5. Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+        driver.findElement(By.xpath("//button[text()='Enable']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until(ExpectedConditions.elementToBeClickable(enableBox));
+        //6. “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+        WebElement itsEnableYazi =driver.findElement(By.xpath("//*[text()=\"It's enabled!\"]"));
+        Assert.assertTrue(itsEnableYazi.isEnabled());
+
+        //7. Textbox’in etkin oldugunu(enabled) dogrulayın.
+        Assert.assertTrue(enableBox.isEnabled());
+
+
+    }
 }
