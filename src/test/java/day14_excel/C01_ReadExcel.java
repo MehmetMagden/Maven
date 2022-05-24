e735e7ded125d2e0a14dfb00b0b0ae891d50c42a
Mehmet Magden <yasamtr@gmail.com>
2022-05-24 21:25:16 +0300

    BackUpp

--- src/test/java/day14_excel/C01_ReadExcel.java
+++ src/test/java/day14_excel/C01_ReadExcel.java
@@ -1,11 +1,32 @@
 package day14_excel;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class C01_ReadExcel {
 
     @Test
-    public void readExcelTest() {
+    public void readExcelTest() throws IOException {
+        //7. Dosya yolunu bir String degiskene atayalim
+        String dosyaYolu="src/resources/ulkeler.xlsx";
+        //8. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis= new FileInputStream(dosyaYolu);
+        //9. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
+        Workbook workbook = WorkbookFactory.create(fis);
+        //10. WorkbookFactory.create(fileInputStream)
+        //11. Sheet objesi olusturun workbook.getSheetAt(index)
+        Sheet sheet = workbook.getSheet("Sayfa1");
+
+        //12. Row objesi olusturun sheet.getRow(index)
+        Row row = sheet.
+        //13. Cell objesi olusturun row.getCell(index)
 
 
 
