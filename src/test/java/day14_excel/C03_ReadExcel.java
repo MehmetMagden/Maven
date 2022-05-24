ba3d746664b53b9328e2233f443746cb2d70887a
Mehmet Magden <yasamtr@gmail.com>
2022-05-24 22:36:44 +0300

    BackUpp

--- src/test/java/day14_excel/C03_ReadExcel.java
+++ src/test/java/day14_excel/C03_ReadExcel.java
@@ -1,4 +1,44 @@
 package day14_excel;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class C03_ReadExcel {
+
+    @Test
+    public void readExcelTest() throws IOException {
+        //input olarak verilen
+        /*
+        satırNo, sutunNo parametrelerini parametre olarak alıp o cell deki datayı String olarak bana döndüren birmethod
+        oluşturun
+         */
+
+        int satirNo=4;
+        int sutunNo=4;
+        //Dönen string'in Cezayir olduğunu test edin
+        String expectedData = "Cezayir";
+
+        String actualData= banaDataGetir(satirNo-1,sutunNo-1);  //cünkü excel endex ile çalışır
+        Assert.assertEquals(expectedData,actualData);
+    }
+
+    public static String banaDataGetir(int satirIndex, int sutunIndex) throws IOException {
+        String istenenData="";
+        String dosyaYolu ="src/resources/ulkeler.xlsx";
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        Workbook workbook = WorkbookFactory.create(fis);
+        istenenData = workbook.getSheet("Sayfa1").getRow(satirIndex).getCell(sutunIndex).toString();
+
+
+
+        return istenenData;
+
+
+    }
 }
---
+++ src/test/java/day14_excel/C04_ReadExcel.java
@@ -0,0 +1,30 @@
+package day14_excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C04_ReadExcel {
+
+    @Test
+    public void readExcelTest() throws IOException {
+        //ingilizce başkentler sutununu yazdıralım
+        String dosyaYolu ="src/resources/ulkeler.xlsx";
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        Workbook workbook = WorkbookFactory.create(fis);
+        int sonSatir = workbook.getSheet("Sayfa1").getLastRowNum();
+        System.out.println("sonSatir = " + sonSatir);
+        String satirdakiData ="";
+
+        for (int i = 0; i <=sonSatir ; i++) {
+            satirdakiData= C03_ReadExcel.banaDataGetir(i,1);
+            System.out.println("satirdakiData = " + satirdakiData);
+
+        }
+
+    }
+}
